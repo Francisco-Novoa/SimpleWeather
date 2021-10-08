efd0d060fa3d6777007ba9e88c854d71645dc055
pancho <fj.novoap@gmail.com>
2021-10-08 04:04:47 -0300

    Adding Utils

---
+++ src/utils.js
@@ -0,0 +1,24 @@
+export const kelvin2Celcius = kelvin => kelvin - 273.15;
+
+export const to2Digits = floatingPointNumber =>
+  Math.trunc(floatingPointNumber * 100) / 100;
+
+export const to12HourClock = time =>
+  new Date(time * 1000).toLocaleDateString("es-CL", {
+    weekday: "short",
+    month: "short",
+    day: "2-digit",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+
+export const weekdayMonthDay = time =>
+  new Date(time * 1000)
+    .toLocaleDateString("en-US", {
+      weekday: "short",
+      month: "short",
+      day: "2-digit",
+    })
+    .split(" ")
+    .join("\u00A0");
