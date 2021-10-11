394fca63220a4e09364af1b9c7554c2b50fb4b22
pancho <fj.novoap@gmail.com>
2021-10-11 11:12:30 -0300

    Layout now uses grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,33 @@
+//library imports
 import React, { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 
+//self made imports
 import Layout from "../components/Layout";
-import { cashedSimpleForecast } from "../services/cashedApi";
-import HourForecast from "../components/HourForecast";
-import DaysForecast from "../components/DaysForecast";
+import {
+  cashedSimpleForecast,
+  cashedCurrentWeather,
+} from "../services/cashedApi";
+import HourForecast from "../components/forecasts/HourForecast";
+import DaysForecast from "../components/forecasts/DaysForecast";
+import CurrentWeather from "../components/forecasts/CurrentWeather";
+import { localStorage } from "../services/storage";
 import Preselected from "../components/Preselected";
 
+//hard data imports
 import cities from "../data/cities.json";
 import preselectedCities from "../data/preselected-cities.json";
 
-const styles = {
-  mainStyle: {
-    margin: "10px",
-  },
-  container: {
-    height: "400px",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-};
+const styles = {};
 
 const defaultSelectedCity = 0;
-
+const howManyCitiesSave = 4;
 export default function Index() {
   const [state, setState] = useState({
     loading: true,
     cities: cities,
     preselectedCities,
+    lastCities: localStorage.retrieve("lastCities") || [],
     selectedCity: preselectedCities[defaultSelectedCity],
     city: preselectedCities[defaultSelectedCity].name,
   });
@@ -35,8 +35,27 @@ export default function Index() {
   const onChange = update =>
     setState(prevState => ({ ...prevState, ...update }));
 
-  const onNamedChange = inputName => update => {
-    onChange({ [inputName]: update.target.textContent });
+  const onSearch = update => {
+    setState(prevState => {
+      const city = update.target.textContent;
+      if (city.length === 0) {
+        return { ...prevState };
+      }
+      const newCity = { name: city };
+      const isRepeated = elem => elem.name === newCity.name;
+      if (
+        prevState.lastCities.some(isRepeated) ||
+        prevState.preselectedCities.some(isRepeated)
+      ) {
+        return { ...prevState, city };
+      }
+      const citiesList = [
+        newCity,
+        ...prevState.lastCities.slice(0, howManyCitiesSave),
+      ];
+      localStorage.save("lastCities", citiesList);
+      return { ...prevState, city, lastCities: citiesList };
+    });
   };
 
   useEffect(() => {
@@ -48,7 +67,10 @@ export default function Index() {
         }));
       }
 
-      const selectedCity = { forecast: await cashedSimpleForecast(state.city) };
+      const selectedCity = {
+        forecast: await cashedSimpleForecast(state.city),
+        currentWeather: await cashedCurrentWeather(state.city),
+      };
 
       setState(prevState => ({
         ...prevState,
@@ -58,17 +80,45 @@ export default function Index() {
     fetchData();
   }, [state.city]);
 
+  useEffect(() => console.log(state), [state]);
+
   return (
     <Layout
       state={state}
       setState={setState}
       onChange={onChange}
-      onNamedChange={onNamedChange}
+      onSearch={onSearch}
     >
-      <Grid sx={styles.mainStyle}>
-        <Preselected state={state} onChange={onChange} />
-        <HourForecast state={state} />
-        <DaysForecast state={state} />
+      <Grid
+        container
+        item
+        sx={{
+          display: "grid",
+          gap: 2,
+          gridTemplateColumns: "repeat(12, 1fr)",
+          gridTemplateRows: "repeat(11, 100px)",
+        }}
+      >
+        <CurrentWeather
+          state={state}
+          sx={{ gridColumn: "1/8", gridRow: "1/4" }}
+        />
+
+        <Preselected
+          onChange={onChange}
+          state={state}
+          sx={{ gridColumn: "8/12", gridRow: "1/4" }}
+        />
+
+        <HourForecast
+          state={state}
+          sx={{ gridColumn: "1/12", gridRow: "4/7" }}
+        />
+
+        <DaysForecast
+          state={state}
+          sx={{ gridColumn: "1/12", gridRow: "7/11" }}
+        />
       </Grid>
     </Layout>
   );
