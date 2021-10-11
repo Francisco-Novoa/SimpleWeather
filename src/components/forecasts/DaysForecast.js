962bad8fc0bd1fe2dd01705e6fdebd4d1b6fd1ff
pancho <fj.novoap@gmail.com>
2021-10-11 11:05:14 -0300

    Moving forecasts to their own forler and improving styles

---
+++ src/components/forecasts/DaysForecast.js
@@ -0,0 +1,255 @@
+import React, { useEffect, useState } from "react";
+import { WiStrongWind } from "react-icons/wi";
+import {
+  Grid,
+  Typography,
+  CircularProgress,
+  Card,
+  Divider,
+} from "@mui/material";
+
+import {
+  weekdayMonthDay,
+  kelvin2Celcius,
+  to2Digits,
+  meterPerS2KmPerHour,
+} from "../../utils";
+import { getIconUrl } from "../../services/getIcon";
+
+const styles = {
+  align: {
+    display: "flex",
+    alignItems: "center",
+    height: "100%",
+  },
+};
+
+const DayForecast = ({ singleForecast, index }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, [singleForecast]);
+
+  return (
+    <Card
+      sx={{
+        gridColumn: index <= 2 ? "span 4" : "span 6",
+        gridRow: "span 2",
+        height: "100%",
+        width: "100%",
+        padding: "20px",
+      }}
+    >
+      {!loading ? (
+        <Grid
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(6,1fr)",
+            gridTemplateRows: "repeat(5, 20%)",
+
+            boxSizing: "border-box",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <Grid sx={{ gridColumn: "1/2", gridRow: "1/4" }}>
+            <Grid
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+              }}
+            >
+              <img
+                alt={singleForecast.weather[0].description}
+                src={getIconUrl(singleForecast.weather[0].icon)}
+              />
+            </Grid>
+          </Grid>
+          <Grid sx={{ gridColumn: "2/5", gridRow: "1/4" }}>
+            <Grid sx={{ ...styles.align, justifyContent: "center" }}>
+              <Typography variant="h6">
+                {weekdayMonthDay(singleForecast.dt)}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid sx={{ gridColumn: "1/2", gridRow: "4/-1" }}>
+            <Grid sx={{ ...styles.align, justifyContent: "center" }}>
+              <Typography sx={{ color: "#878787", textAlign: "center" }}>
+                {singleForecast.weather[0].description
+                  .split(" ")
+                  .join("\u00A0")}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid sx={{ gridColumn: "2/5", gridRow: "4/6" }}>
+            <Grid sx={{ ...styles.align, justifyContent: "center" }}>
+              <Typography
+                sx={{
+                  color: "#878787",
+                }}
+                variant="subtitle1"
+              >
+                Max:&nbsp;
+              </Typography>
+              <Typography variant="subtitle1">
+                {to2Digits(kelvin2Celcius(singleForecast.main.temp_max))}°C
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid sx={{ gridColumn: "5/-1", gridRow: "4/6" }}>
+            <Grid
+              sx={{
+                ...styles.align,
+                justifyContent: "center",
+                paddingLeft: "5px",
+              }}
+            >
+              <Typography
+                sx={{
+                  color: "#878787",
+                }}
+                variant="subtitle1"
+              >
+                Min:&nbsp;
+              </Typography>
+              <Typography variant="subtitle1">
+                {to2Digits(kelvin2Celcius(singleForecast.main.temp_min))}°C
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid sx={{ gridColumn: "5/-1", gridRow: "1" }}>
+            <Grid
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              <Typography
+                sx={{
+                  color: "#5fb2ce",
+                }}
+              >
+                <WiStrongWind size={30} />
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid sx={{ gridColumn: "5/-1", gridRow: "2/4" }}>
+            <Grid
+              sx={{
+                display: "flex",
+                alignItems: "start",
+                justifyContent: "center",
+              }}
+            >
+              <Typography sx={{ color: "#5fb2ce" }} variant="subtitle1">
+                {to2Digits(meterPerS2KmPerHour(singleForecast.wind.speed))}
+                &nbsp;
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                sx={{
+                  color: "#5fb2ce",
+                }}
+              >
+                km/hr
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      ) : (
+        <Grid
+          container
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CircularProgress
+            variant="indeterminate"
+            sx={{
+              color: "#fe5800",
+            }}
+          />
+        </Grid>
+      )}
+    </Card>
+  );
+};
+
+//this numers are the indexes that are each 24 hours in the future in the forecast
+const relevantPositionsInArray = [7, 15, 23, 31, 39];
+
+const ForecastContainer = ({ selectedCity }) => {
+  const forecastList = selectedCity?.forecast?.list;
+  const relevantForecasts =
+    forecastList?.length &&
+    relevantPositionsInArray.map(index => forecastList[index]);
+  return (
+    <Grid
+      sx={{
+        height: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(12,1fr)",
+        gridTemplateRows: "repeat(12,1fr)",
+        padding: "16px",
+      }}
+      justifyContent="center"
+    >
+      <Grid sx={{ gridColumn: "1/-1", gridRow: "1/3" }}>
+        <Typography sx={{ marginBottom: "16px" }} variant="h5">
+          Next Five Days
+        </Typography>
+        <Divider />
+      </Grid>
+      <Grid
+        sx={{
+          gridColumn: "1/24",
+          gridRow: "3/12",
+          width: "100%",
+        }}
+      >
+        <Grid
+          sx={{
+            height: "100%",
+            display: "grid",
+            gridTemplateColumns: "repeat(12,1fr)",
+            gridTemplateRows: "repeat(4,1fr)",
+            gap: 1,
+            justifyContent: "center",
+          }}
+        >
+          {relevantForecasts?.map((elem, index) => {
+            return (
+              <DayForecast singleForecast={elem} key={elem.dt} index={index} />
+            );
+          })}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default function DaysForecast({ state, sx }) {
+  return (
+    <Card sx={sx}>
+      <ForecastContainer
+        selectedCity={state?.selectedCity}
+        loading={state.loading}
+      />
+    </Card>
+  );
+}
---
+++ src/components/forecasts/HourForecast.js
@@ -0,0 +1,219 @@
+import React, { useEffect, useState } from "react";
+import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
+import {
+  Grid,
+  Typography,
+  Card,
+  Divider,
+  Button,
+  CircularProgress,
+} from "@mui/material";
+
+import { kelvin2Celcius, to2Digits, to12HourClock } from "../../utils";
+import { getIconUrl } from "../../services/getIcon";
+
+const HourForecast = ({ singleForecast }) => {
+  const [loading, setLoading] = useState(true);
+  const [forecast, setForecast] = useState(singleForecast);
+
+  useEffect(() => {
+    setLoading(true);
+    setForecast(singleForecast);
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, [singleForecast]);
+
+  return (
+    <Card
+      variant="outlined"
+      sx={{
+        height: "100%",
+      }}
+    >
+      {!loading ? (
+        <Grid
+          containter
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "16px",
+            justifyContent: "space-between",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <Grid item>
+            <Typography sx={{ color: "#358eac" }} variant="subtitle2">
+              humidity&nbsp;{forecast.main.humidity}%
+            </Typography>
+          </Grid>
+          <Grid item container sx={{ justifyContent: "center" }}>
+            <Grid item sx={{ color: "#fe5800" }}>
+              <Typography variant="h4">
+                {to2Digits(kelvin2Celcius(forecast.main.temp))}°C
+              </Typography>
+            </Grid>
+            <Grid item sx={{ alignContent: "center" }}>
+              <img alt="" src={getIconUrl(forecast.weather[0].icon)} />
+            </Grid>
+          </Grid>
+          <Grid item sx={{ textAlign: "center", justifyContent: "center" }}>
+            <Typography sx={{ color: "#878787" }}>
+              {to12HourClock(forecast.dt)}
+            </Typography>
+          </Grid>
+        </Grid>
+      ) : (
+        <Grid
+          container
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <CircularProgress
+            variant="indeterminate"
+            sx={{
+              color: "#fe5800",
+            }}
+          />
+        </Grid>
+      )}
+    </Card>
+  );
+};
+
+const initialForecast = 0;
+const lastForecast = 3;
+
+const ForecastContainer = ({ selectedCity, loading }) => {
+  const [localState, setBounds] = useState({
+    list: [],
+    left: initialForecast,
+    right: lastForecast,
+  });
+
+  useEffect(() => {
+    setBounds(prevState => ({
+      ...prevState,
+      list: selectedCity?.forecast?.list?.slice(initialForecast, lastForecast),
+    }));
+  }, [selectedCity]);
+
+  const onClickPrev = () =>
+    setBounds(prevState => ({
+      left: prevState.left - 1,
+      right: prevState.right - 1,
+      list: selectedCity?.forecast?.list?.slice(
+        prevState.left - 1,
+        prevState.right - 1
+      ),
+    }));
+
+  const onClickNext = () =>
+    setBounds(prevState => ({
+      right: prevState.right + 1,
+      left: prevState.left + 1,
+      list: selectedCity?.forecast?.list?.slice(
+        prevState.left + 1,
+        prevState.right + 1
+      ),
+    }));
+
+  return (
+    <Grid
+      sx={{
+        height: "100%",
+        width: "100%",
+        padding: "16px",
+        display: "grid",
+        gap: 1,
+        gridTemplateColumns: "repeat(11, 1fr)",
+        gridTemplateRows: "repeat(8, 12.5%)",
+      }}
+    >
+      <Grid sx={{ gridColumn: "1/-1", gridRow: "1" }}>
+        <Typography sx={{ marginBottom: "16px" }} variant="h5">
+          Next Hours
+        </Typography>
+        <Divider />
+      </Grid>
+
+      <Grid sx={{ gridColumn: "1", gridRow: "3/7" }}>
+        <Button
+          onClick={onClickPrev}
+          disabled={localState.left === 0}
+          variant="outlined"
+          sx={{
+            minWidth: "100%",
+            minHeight: "100%",
+            color: "#878787",
+            borderColor: "#878787",
+            "&:hover": { borderColor: "#878787", bgcolor: "#fe5800" },
+          }}
+        >
+          <BiLeftArrow />
+        </Button>
+      </Grid>
+
+      {loading ? (
+        localState.list?.map((elem, index) => {
+          const one = "2/5";
+          const two = "5/8";
+          const three = "8/11";
+          return (
+            <Grid
+              sx={{
+                gridColumn: `${index === 0 ? one : index === 1 ? two : three}`,
+                gridRow: "3/7",
+              }}
+            >
+              <HourForecast
+                index={index}
+                singleForecast={elem}
+                loading={loading}
+                key={elem.dt}
+              />
+            </Grid>
+          );
+        })
+      ) : (
+        <CircularProgress color="secondary" />
+      )}
+
+      <Grid sx={{ gridColumn: "11", gridRow: "3/7" }}>
+        <Button
+          onClick={onClickNext}
+          variant="outlined"
+          disabled={
+            localState.right ===
+            selectedCity?.forecast?.list?.length - lastForecast
+          }
+          sx={{
+            minWidth: "100%",
+            minHeight: "100%",
+            borderColor: "#878787",
+            "&:hover": { borderColor: "#878787" },
+          }}
+          disableRipple
+        >
+          <BiRightArrow style={{ color: "#878787" }} />
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default function HoursForecast({ state, sx }) {
+  return (
+    <Card sx={sx}>
+      <ForecastContainer
+        selectedCity={state?.selectedCity}
+        loading={state.loading}
+      />
+    </Card>
+  );
+}
